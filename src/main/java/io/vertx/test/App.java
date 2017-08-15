256ba91c536d949cfc76422044a8d1d10f979f55
Ralitera Solofo <solofo.ralitera@gmail.com>
2017-08-15 14:38:49 +0300

    Route

--- src/main/java/io/vertx/test/App.java
+++ src/main/java/io/vertx/test/App.java
@@ -1,7 +1,12 @@
 package io.vertx.test;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 /**
  * Hello world!
@@ -11,24 +16,31 @@ public class App extends AbstractVerticle
 {
     public static void main( String[] args )
     {
-
         System.out.println( "Hello World!" );
     }
 
     @Override
-    public void start(Future<Void> fut) {
+    public void start(Future<Void> startFuture) {
+        final Router router = Router.router(vertx);
+
+        router.get("/").handler((RoutingContext req) -> req.response().end(""));
+
+        router.get("/hello/")
+            .handler((RoutingContext req) -> req.response()
+                .putHeader("content-type", "text/html")
+                .end("<html><body><h1>Hello World</h1></body></html>")
+            );
+
         vertx
-                .createHttpServer()
-                .requestHandler(r -> {
-                    r.response().end("Hello world");
-                })
-                .listen(8080, result -> {
-                    if (result.succeeded()) {
-                        fut.complete();
-                    } else {
-                        fut.fail(result.cause());
-                    }
-                });
+            .createHttpServer()
+            .requestHandler(router::accept)
+            .listen(8080, (AsyncResult<HttpServer> result) -> {
+                if (result.succeeded()) {
+                    startFuture.complete();
+                } else {
+                    startFuture.fail(result.cause());
+                }
+            });
     }
 
 }
