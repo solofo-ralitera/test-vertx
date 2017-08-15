252b3868c80df6806be7802a54f7bcc0fcc7ef05
Ralitera Solofo <solofo.ralitera@gmail.com>
2017-08-15 16:35:40 +0300

    Deployment

---
+++ src/main/java/io/vertx/test/Api.java
@@ -0,0 +1,42 @@
+package io.vertx.test;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+
+public class Api extends AbstractVerticle
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    @Override
+    public void start(Future<Void> startFuture) {
+        final Router router = Router.router(vertx);
+
+        router.get("/api").handler((RoutingContext req) -> req.response().end(""));
+
+        router.get("/api/hello/")
+                .handler((RoutingContext req) -> req.response()
+                        .putHeader("content-type", "application/json")
+                        .end("{ \"message\" : \"Hello World!\" }")
+                );
+
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(8080, (AsyncResult<HttpServer> result) -> {
+                    if (result.succeeded()) {
+                        startFuture.complete();
+                    } else {
+                        startFuture.fail(result.cause());
+                    }
+                });
+    }
+
+}
--- src/main/java/io/vertx/test/App.java
+++ src/main/java/io/vertx/test/App.java
@@ -1,12 +1,6 @@
 package io.vertx.test;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 
 /**
  * Hello world!
@@ -20,27 +14,10 @@ public class App extends AbstractVerticle
     }
 
     @Override
-    public void start(Future<Void> startFuture) {
-        final Router router = Router.router(vertx);
+    public void start() throws Exception {
 
-        router.get("/").handler((RoutingContext req) -> req.response().end(""));
+        vertx.deployVerticle(Api.class.getCanonicalName());
 
-        router.get("/hello/")
-            .handler((RoutingContext req) -> req.response()
-                .putHeader("content-type", "text/html")
-                .end("<html><body><h1>Hello World</h1></body></html>")
-            );
-
-        vertx
-            .createHttpServer()
-            .requestHandler(router::accept)
-            .listen(8080, (AsyncResult<HttpServer> result) -> {
-                if (result.succeeded()) {
-                    startFuture.complete();
-                } else {
-                    startFuture.fail(result.cause());
-                }
-            });
     }
 
 }
